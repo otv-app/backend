0b1638c4cfcb25f8fb26e11eb711f54d0c5655a7
Patrick Yoon <patrickyoon@Patricks-Mac.local>
2020-06-26 16:26:08 -0700

    init commit :)

---
+++ OTVModel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OTVModel
+//
+//  Created by Patrick Yoon on 6/26/20.
+//  Copyright © 2020 Patrick Yoon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
